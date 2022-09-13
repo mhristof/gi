51d57d95663dfa9b86e175d42071fb156cc22708
Mike Christofilopoulos <mhristof@gmail.com>
2022-09-13 15:41:20 +0100

    fix: dont fail if config is missing

--- cmd/root.go
+++ cmd/root.go
@@ -89,7 +89,8 @@ func initConfig() {
 	}
 
 	if err := viper.ReadInConfig(); err != nil {
-		fmt.Println("Can't read config:", err)
-		os.Exit(1)
+		log.WithFields(log.Fields{
+			"err": err,
+		}).Warning("cannot read config")
 	}
 }
